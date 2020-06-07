a6b51fab8501f04a8f5c62cf984d30c3887d5657
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2020-06-07 18:56:43 +0800

    0.1.1

--- pypat/signer.py
+++ pypat/signer.py
@@ -3,8 +3,7 @@ from abc import ABC, abstractmethod, abstractstaticmethod
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric.ed25519 import (
-    Ed25519PrivateKey, Ed25519PublicKey)
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
 from cryptography.hazmat.primitives.hmac import HMAC, hashes
 
 __all__ = ['BaseSigner', 'hmac', 'ed25519', 'ed25519_v']
