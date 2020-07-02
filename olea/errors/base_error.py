3f3b522b3d3c57af4f5e0af8e80dc529a30f8aaf
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2020-07-02 09:35:29 +0800

    fix

--- olea/errors/base_error.py
+++ olea/errors/base_error.py
@@ -7,20 +7,16 @@ class BaseError(Exception):
     http_code = 500
     code = ''
 
-    def __init_subclass__(cls):
-        cls.pre = re.sub(r'(?!^)([A-Z])', r' \1', cls.__name__) + ' - '
-        return super().__init_subclass__()
+    def __new__(cls, *args, **kwargs):
+        pre = re.sub(r'(?!^)([A-Z])', r' \1', cls.__base__.__name__)
+        cls.msg = pre + ' - ' + re.sub(r'(?!^)([A-Z])', r' \1', cls.__name__)
+        return super().__new__(cls, *args, **kwargs)
 
     def __init__(self, **kwargs):
         self.parms = kwargs
         super().__init__()
 
     def __json__(self):
-        d = {
-            'code': self.code,
-            'ref': g.ref,
-            'msg': re.sub(r'(?!^)([A-Z])', r' \1', self.__class__.__name__),
-            'parms': self.parms
-        }
+        d = {'code': self.code, 'ref': g.ref, 'msg': self.msg, 'parms': self.parms}
 
         return d
