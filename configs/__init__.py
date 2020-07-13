8adc8977604f06e39e5ba43d6f811f2d50ee7771
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2020-07-13 09:10:21 +0800

    rename

--- configs/__init__.py
+++ configs/__init__.py
@@ -4,9 +4,6 @@ import os
 def load_config(env=os.getenv('FLASK_ENV', 'production')):
     from werkzeug.utils import import_string
 
-    if env == 'production':
-        module = import_string('configs.base_config')
-    else:
-        module = import_string(f'configs.{env}')
+    module = import_string(f'configs.{env}')
 
     return module.Config()
